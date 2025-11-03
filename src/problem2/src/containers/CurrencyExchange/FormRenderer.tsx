bce0e5c21faebda7ae6fef5ff3abda62702ca4aa
Thai (Nisil) Nguyen <nisil.thaing@gmail.com>
2025-11-04 02:18:37 +0700

    [Problem 2]: Add swap icon between 2 fields in the Currency Exchange form

--- src/problem2/src/containers/CurrencyExchange/FormRenderer.tsx
+++ src/problem2/src/containers/CurrencyExchange/FormRenderer.tsx
@@ -15,6 +15,11 @@ type FormRendererProps = FormikProps<CurrencyExchangeFormData> & {
 const CurrencyAmountFieldInput = CurrencyAmountField<CurrencyExchangeFormData>;
 
 export const FormRenderer: FC<FormRendererProps> = ({ unitOptions, isSubmitting, dirty, isValid, handleSubmit }) => {
+  const handleSwapFromTo = () => {
+    // TODO: For improvement:
+    // When users click on this button, the From and To field would be swapped
+  };
+
   return (
     <form className="p-4" onSubmit={handleSubmit}>
       <FieldGroup>
@@ -30,6 +35,30 @@ export const FormRenderer: FC<FormRendererProps> = ({ unitOptions, isSubmitting,
               unitOptions={unitOptions}
               isDisabled={isSubmitting}
             />
+            <div className="text-center">
+              <Button
+                type="button"
+                variant="outline"
+                size="icon"
+                className="rounded-full not-disabled:cursor-pointer"
+                onClick={handleSwapFromTo}
+                disabled // temporary disabled to improve if any chances
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down-up"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5m-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5"
+                  />
+                </svg>
+              </Button>
+            </div>
             <CurrencyAmountFieldInput
               name="to"
               fieldLabel="To"
